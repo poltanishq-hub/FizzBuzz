0be9880e85a35b738518861193f450fd7edb5060
poltanishq-hub <pol.tanishq@gmail.com>
2026-09-14 11:51:09 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,10 +7,16 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-
+        int i=1;
+        while(i<=100){
             doFizzBuzz(i);
+            i++;
         }
+
+
+//        for (int i = 1; i <= 100; i++) {
+//            doFizzBuzz(i);
+//        }
     }
 
     public static void doFizzBuzz(int i) {
